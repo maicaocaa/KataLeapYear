4b14323b2be1bee29fa5d047c0764f9cb3ffbdb8
maicaocaa <maria.caocaa@gmail.com>
2024-01-26 15:39:01 +0100

    leapYearInOneFunction test ok

--- KataLeapYear/LeapYear.js
+++ KataLeapYear/LeapYear.js
@@ -1,11 +1,21 @@
 class LeapYear {
-    divisibleBy4 (num) {
-        return num % 4 === 0 ? true:false;
-    }
-    divisibleBy400 (num) {
-        return num % 400 === 0 ? true:false;
-    }
+    // divisibleBy4 (num) {
+    //     return num % 4 === 0 ;
+    // }
 
-}
+    //  divisibleBy400(num) {
+    //      return num % 400 === 0;
+    //  }
+
+    // divisibleBy100and400 (num) {
+    //    return num % 100 === 0 ? this.divisibleBy400(num): false;
+    // }
+
+    leapYearInOneFunction (num) { 
+       return (num % 4 === 0 && num % 100 !==0) || (num %400 === 0)?true:false;
+              
+       }
+     }
+    
 
 module.exports = LeapYear;
--- KataLeapYear/test/index.test.js
+++ KataLeapYear/test/index.test.js
@@ -9,31 +9,42 @@ const LeapYear = require ("../LeapYear.js");
     * Un año es bisiesto si es divisible por 400
     * Un año no es bisiesto si es divisible por 100 pero no por 400  
    * true si es bisiesto y false si no lo es 
+   * 
+   *1997 no es bisiesto (no es divisible por 4)
+   1996 es bisiesto (divisible por 4)
+   1600 es bisiesto (divisible por 400)
+   1800 no es bisiesto (divisible por 4, divisible por 100, NO divisible por 400)
  */
 
 describe ('Test que valida año bisiesto', () => { 
    const leap_year = new LeapYear();
 
    test ('test que devuelve false si el año no es divisible por 4',()=> {
-      const year = 1995;
+      const year = 1997;
       const respuesta_esperada = false;
-      const respuesta_recibida = leap_year.divisibleBy4 (year);
+      const respuesta_recibida = leap_year.  leapYearInOneFunction  (year);
       expect (respuesta_recibida).toBe(respuesta_esperada);
    })
 
-   test ('test que devuelve true si el año  es divisible por 4',()=> {
-      const year = 2000;
+   test ('test que devuelve true si el año es divisible por 4',()=> {
+      const year = 1996;
       const respuesta_esperada = true;
-      const respuesta_recibida = leap_year.divisibleBy4 (year);
+      const respuesta_recibida = leap_year.  leapYearInOneFunction  (year);
       expect (respuesta_recibida).toBe(respuesta_esperada);
    })
   
-   test ('test que devuelve true si el año  es divisible por 400',()=> {
-      const year = 2000;
+   test ('test que devuelve true si el año es divisible por 400',()=> {
+      const year = 1600;
       const respuesta_esperada = true;
-      const respuesta_recibida = leap_year.divisibleBy4 (year);
+      const respuesta_recibida = leap_year.  leapYearInOneFunction  (year);
       expect (respuesta_recibida).toBe(respuesta_esperada);
    })
 
+   test ('Test que devuelve false si el año es divisible por 100 pero no por 400',()=> {
+      const year = 1800;
+      const respuesta_esperada = false;
+      const respuesta_recibida = leap_year.  leapYearInOneFunction  (year);
+      expect (respuesta_recibida).toBe(respuesta_esperada);
+   })
 
 });
